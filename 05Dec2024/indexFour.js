4aa17ffab46249ecebbbb16b61b4ae8eacde6230
Dineshkumar <dineshkumar.arul@gmail.com>
2024-12-05 22:37:15 +0530

    5th dec 2024

---
+++ 05Dec2024/indexFour.js
@@ -0,0 +1,6 @@
+
+import sayHello ,{ userName  }  from "./indexThree.js";
+
+console.log("username is : ", userName);
+
+sayHello();
---
+++ 05Dec2024/indexThree.js
@@ -0,0 +1,13 @@
+
+export  const userName = "Vinod";
+
+export default function sayHello(){
+
+    console.log(" Hello ");
+}
+
+// default shouls only one and we can have more than one non default in javascript
+// non default is called as named parameters / exports
+// ESM - Ecma script module in Node environment .
+
+
